a53d3adef058f3674d33f5f295c3a04bb339aab3
Culip Blue <ridhohafiz98@gmail.com>
2025-06-25 22:10:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Serve static files from root (so index.html, images, scripts, style, assets, dll bisa diakses)
+app.use(express.static(__dirname));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
